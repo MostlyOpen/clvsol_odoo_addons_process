20a9d105dd4fe7aff7f149f3db80fd6652ff018b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-04-02 14:34:02 -0300

    [ADD] clv_survey_process

---
+++ clv_survey_process/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Survey Process',
+    'summary': 'Survey Process Module used by CLVsol Solutions.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'images': [],
+    'depends': [
+        'clv_processing',
+        'clv_survey',
+    ],
+    'data': [
+        'data/survey_process_yaml.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
