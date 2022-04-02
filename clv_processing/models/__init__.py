cb746e99baaf141dad961211e6ae44f1a8bde50c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-04-02 14:33:38 -0300

    [REF] clv_processing

--- clv_processing/models/__init__.py
+++ clv_processing/models/__init__.py
@@ -3,14 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import processing_host
-from . import processing_host_log
 from . import processing_template
-from . import processing_template_log
 from . import processing_schedule
-from . import processing_schedule_log
-# from . import processing_batch
-# from . import processing_batch_member
-# from . import processing_batch_log
-# from . import referenceable_model
-
+# # from . import processing_batch
+# # from . import processing_batch_member
+# # from . import referenceable_model
 from . import abstract_process
